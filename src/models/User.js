d7c1f05cb6164839e8f60c9c0b290b13263c2bb8
shazid25 <ishazid57@gmail.com>
2025-10-14 01:35:54 +0600

    Working on database

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 
 const userSchema = new mongoose.Schema({
    fullName:{
@@ -40,4 +41,33 @@ const userSchema = new mongoose.Schema({
         type: Boolean,
         default: false,
     },
+    friends:[
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+        }
+    ]
 }, {timestamps: true});
+
+
+
+userSchema.pre('save', async function(next){
+    if(!this.isModified('password')){
+        return next();
+    }
+try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+
+    next();
+} catch (error) {
+   next(error);
+}
+});
+
+
+const User = mongoose.model('User', userSchema);
+
+
+
+export default User;
--- src/routes/controllers/auth.controller.js
+++ src/routes/controllers/auth.controller.js
@@ -1,5 +1,56 @@
+import USer from "../../models/User.js";
+import jwt from "jsonwebtoken"
+
+
 export async function signup(req, res) {
-    res.send("Signup Route");
+const { email, password, fullName} = req.body;
+
+try {
+if(!email || !password || !fullName){
+    return res.status(400).json({message: "All fields are required"});
+}
+if(password.length < 6){
+    return res.status(400).json({message: "Password must be at least 6 characters"});
+}
+
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+if(!emailRegex.test(email)){
+    return res.status(400).json({message: "Invalid email format"});
+}
+const existingUser = await USer.findOne({email});
+if(existingUser){
+    return res.status(400).json({message: "Email already exists, please use a different one "});
+}
+
+const idx = Math.floor(Math.random() * 100)+1;
+const randomAvatar = `https://avatar.iran.liara.run/public/${idx}.png`;
+
+const newUser = await User.create({
+    email,
+    password,
+    fullName,
+    profilePic: randomAvatar,
+});
+
+const token = jwt.sign({ userId: newUser._id}, process.env.JWT_SECRET_KEY, {
+    expiresIn: '7d',
+});
+
+res.cookie('jwt', token, {
+    maxAge : 7*24*60*60*1000,
+    httpOnly: true,
+    sameSite : 'strict',
+    secure : process.env.NODE_ENV === 'production'
+});
+
+res.status(201).json({success: true, user: newUser});
+
+
+} catch (error) {
+    console.error("Error during signup:", error);
+    res.status(500).json({message: "Internal Server Error"});
+}
+
 }
 
 export async function login(req, res) {
